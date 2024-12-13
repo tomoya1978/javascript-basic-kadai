cd7d25d2a25dac28e0552cfe9367f6b40d3e1898
tomoya1978 <118785358+tomoya1978@users.noreply.github.com>
2024-12-13 16:29:06 +0900

    課題　四則演算をしようの提出です

---
+++ kadai_005/bmi.js
@@ -0,0 +1,16 @@
+// 変数の宣言・値の代入
+let Taijyu = 68;
+let Sintyou = 1.7;
+let BMI;
+
+// コンソールへの出力
+//console.log(Taijyu);
+//console.log(Sintyou);
+
+BMI = Taijyu / (Sintyou * Sintyou);
+
+console.log(BMI);
+
+
+
+
