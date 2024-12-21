41fc28d781a13b5b32bbe10229778831a526f84f
tomoya1978 <118785358+tomoya1978@users.noreply.github.com>
2024-12-21 17:59:32 +0900

    課題　関数をMDNで調べて使ってみよう　の提出です

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+var today = new Date();
+
+//console.log(today);
+
+
+var year = today.getFullYear();
+var month = today.getMonth() + 1;
+var day = today.getDate();
+
+console.log( year + '年' + month + '月' + day + '日');
